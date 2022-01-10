75c555d5eeee2d4b66f9056be32e8debb6c00301
Andrew Kachnic <ajkachnic@protonmail.com>
2022-01-09 20:15:02 -0500

    final scene!

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,8 @@ const Point3 = common.Point3;
 const Sphere = common.Sphere;
 const Vec3 = common.Vec3;
 
+const randomFloat = common.randomFloat;
+
 // materials
 const Dielectric = common.Dieletric;
 const Metal = common.Metal;
@@ -42,47 +44,99 @@ fn rayColor(r: Ray, world: *Hittable, depth: u32) Color {
         .add(Color.init(0.5, 0.7, 1.0).mul(t));
 }
 
+fn randomScene(allocator: std.mem.Allocator) !HittableList {
+    var world = HittableList.init(allocator);
+
+    var groundMaterial = Lambertian.init(Color.init(0.8, 0.8, 0.0));
+    var ground = Sphere.init(Point3.init(0, -1000, -1), 1000, &groundMaterial.interface);
+    try world.append(&ground.interface);
+
+    var a: isize = -11;
+    while (a < 11) : (a += 1) {
+        var b: isize = -11;
+        while (b < 11) : (b += 1) {
+            const center = Point3.init(
+                @intToFloat(f64, a) + 0.9 * randomFloat(f64),
+                0.2,
+                @intToFloat(f64, b) + 0.9 * randomFloat(f64),
+            );
+
+            if (center.sub(Point3.init(4, 0.2, 0)).length() > 0.9) {
+                const _r = common.rnd.random().float(f64);
+                const MatType = enum { Diffuse, Metal, Glass };
+                var choose_mat: MatType = if (_r < 0.8)
+                    MatType.Diffuse
+                else if (_r < 0.95)
+                    MatType.Metal
+                else
+                    MatType.Glass;
+
+                switch (choose_mat) {
+                    .Diffuse => {
+                        var material = try allocator.create(Lambertian);
+                        const albedo = Color.random().mul(Color.random());
+                        material.* = Lambertian.init(albedo);
+
+                        var sphere = try allocator.create(Sphere);
+                        sphere.* = Sphere.init(center, 0.2, &material.interface);
+
+                        try world.append(&sphere.interface);
+                    },
+                    .Metal => {
+                        var material = try allocator.create(Metal);
+                        const albedo = Color.random().mul(Color.random());
+                        const fuzz = randomFloat(f64) / 2;
+                        material.* = Metal.init(albedo, fuzz);
+
+                        var sphere = try allocator.create(Sphere);
+                        sphere.* = Sphere.init(center, 0.2, &material.interface);
+
+                        try world.append(&sphere.interface);
+                    },
+                    .Glass => {
+                        var material = try allocator.create(Dielectric);
+                        material.* = Dielectric.init(1.5);
+
+                        var sphere = try allocator.create(Sphere);
+                        sphere.* = Sphere.init(center, 0.2, &material.interface);
+
+                        try world.append(&sphere.interface);
+                    },
+                }
+            }
+        }
+    }
+
+    return world;
+}
+
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     var allocator = gpa.allocator();
 
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
     // image
     const aspectRatio = 16.0 / 9.0;
     const imageWidth = 1920;
     const imageHeight = @floatToInt(comptime_int, @intToFloat(f64, imageWidth) / aspectRatio);
-    const samplesPerPixel = 200; // 100
-    const maxDepth = 100; // 50
+    const samplesPerPixel = 100; // 100
+    const maxDepth = 50; // 50
 
     std.log.debug("image width: {d}, image height: {d}\n", .{ imageWidth, imageHeight });
 
     // world
-    // const R = std.math.cos(@floatCast(f64, std.math.pi / 4.0));
-    var world = HittableList.init(allocator);
+    var world = try randomScene(arena.allocator());
     defer world.deinit();
 
-    var materialGround = Lambertian.init(Color.init(0.8, 0.8, 0.0));
-    // var materialCenter = Lambertian.init(Color.init(0.1, 0.2, 0.5));
-    var materialCenter = Metal.init(Color.init(0.15, 0.3, 0.7), 0.1);
-    var materialLeft = Metal.init(Color.init(0.8, 0.6, 0.2), 0.0);
-    var materialRight = Dielectric.init(1.5);
-
-    var sphere_a = Sphere.init(Point3.init(0, -100.5, -1), 100, &materialGround.interface);
-    var sphere_b = Sphere.init(Point3.init(0.0, 0.0, -1.0), 0.5, &materialCenter.interface);
-    var sphere_c = Sphere.init(Point3.init(-1.0, 0.0, -1.0), 0.5, &materialLeft.interface);
-    var sphere_d = Sphere.init(Point3.init(1.0, 0.0, -1.0), 0.5, &materialRight.interface);
-
-    try world.append(&sphere_a.interface);
-    try world.append(&sphere_b.interface);
-    try world.append(&sphere_c.interface);
-    try world.append(&sphere_d.interface);
-
     // camera
-    const lookfrom = Point3.init(-1.5, 2, 4);
-    const lookat = Point3.init(0, 0, -1);
+    const lookfrom = Point3.init(13, 2, 3);
+    const lookat = Point3.init(0, 0, 0);
     const vup = Vec3.init(0, 1, 0);
-    const distToFocus = Vec3.sub(lookfrom, lookat).length();
-    const aperture = 2.0;
+    const distToFocus = 10.0;
+    const aperture = 0.1;
 
     var cam = Camera.init(
         lookfrom,
@@ -110,8 +164,8 @@ pub fn main() anyerror!void {
             var pixelColor = Color.init(0, 0, 0);
             var s: usize = 0;
             while (s < samplesPerPixel) : (s += 1) {
-                const u = (@intToFloat(f64, i) + common.randomFloat(f64)) / @intToFloat(f64, imageWidth - 1);
-                const v = (@intToFloat(f64, j) + common.randomFloat(f64)) / @intToFloat(f64, imageHeight - 1);
+                const u = (@intToFloat(f64, i) + randomFloat(f64)) / @intToFloat(f64, imageWidth - 1);
+                const v = (@intToFloat(f64, j) + randomFloat(f64)) / @intToFloat(f64, imageHeight - 1);
                 var r = cam.getRay(u, v);
                 pixelColor = Color.add(pixelColor, rayColor(r, &world.interface, maxDepth));
             }
